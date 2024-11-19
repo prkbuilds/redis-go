3a2157705607dc2d561019879c0feb34ae07ae49
Pratik Fandade <fandadep6@gmail.com>
2024-11-19 01:25:52 -0500

    Reading file content

--- app/server.go
+++ app/server.go
@@ -49,6 +49,39 @@ func (s *Store) Get(key string) (string, bool) {
   return value, exists
 }
 
+const (
+	opCodeModuleAux    byte = 247 /* Module auxiliary data. */
+	opCodeIdle         byte = 248 /* LRU idle time. */
+	opCodeFreq         byte = 249 /* LFU frequency. */
+	opCodeAux          byte = 250 /* RDB aux field. */
+	opCodeResizeDB     byte = 251 /* Hash table resize hint. */
+	opCodeExpireTimeMs byte = 252 /* Expire time in milliseconds. */
+	opCodeExpireTime   byte = 253 /* Old expire time in seconds. */
+	opCodeSelectDB     byte = 254 /* DB number of the following keys. */
+	opCodeEOF          byte = 255
+)
+
+func sliceIndex(data []byte, sep byte) int {
+	for i, b := range data {
+		if b == sep {
+			return i
+		}
+	}
+	return -1
+}
+
+func parseTable(bytes []byte) []byte {
+	start := sliceIndex(bytes, opCodeResizeDB)
+	end := sliceIndex(bytes, opCodeEOF)
+	return bytes[start+1 : end]
+}
+func readFile(path string) string {
+	c, _ := os.ReadFile(path)
+	key := parseTable(c)
+	str := key[4 : 4+key[3]]
+	return string(str)
+}
+
 func (s *Store) expireKeyAfter(key string, expiry int) {
   time.Sleep(time.Duration(expiry) * time.Millisecond)
   s.mu.Lock()
@@ -148,6 +181,11 @@ func handleConnection(conn net.Conn, s *Store) {
             default:
               conn.Write([]byte("$-1\r\n"))
           }
+        }
+      case "KEYS":
+        if command[1] == "*" {
+          fileContent := readFile(dir + "/" + dbfilename)
+          conn.Write([]byte(ToRESP([]string{fileContent})))
         }
       default:
         conn.Write([]byte("ERROR: unknown command\n"))
