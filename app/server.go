cba88eb945b00665094ab2f2165493aa6e9bf1c9
Pratik Fandade <fandadep6@gmail.com>
2024-11-21 16:19:43 -0500

    Make the Psync command to master

--- app/server.go
+++ app/server.go
@@ -47,6 +47,8 @@ func (s *Server) Run() error {
 		m.Write([]byte("*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n6380\r\n"))
 		time.Sleep(1 * time.Second)
 		m.Write([]byte("*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n"))
+		time.Sleep(1 * time.Second)
+    m.Write([]byte("*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n"))
   }
   args := os.Args
   for i := 1; i < len(args); i++ {
