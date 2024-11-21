1dd2cfe33e674cf33eb084c9894cf5ed29ea37e9
Pratik Fandade <fandadep6@gmail.com>
2024-11-21 16:56:20 -0500

    Making a empty db pass to the master response

--- app/client_handler.go
+++ app/client_handler.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"context"
 	"fmt"
+  "encoding/hex"
 	"io"
 	"os"
 	"path/filepath"
@@ -18,6 +19,8 @@ const (
 	okResponse   = "+OK\r\n"
 	nullResponse = "$-1\r\n"
 )
+
+var emptyRDB, _ = hex.DecodeString("524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2")
 
 type ClientHandler struct {
 	Context context.Context
@@ -132,7 +135,8 @@ func (c *ClientHandler) executeCommand(cmd Command) error {
   case "REPLCONF":
     return c.send(okResponse)
   case "PSYNC":
-    return c.send(encodeBulkString("FULLRESYNC 8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb 0"))
+    c.send(encodeBulkString("FULLRESYNC 8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb 0"))
+    return c.send(fmt.Sprintf("$%d\r\n%s", len(emptyRDB), emptyRDB))
 	default:
 		return fmt.Errorf("unrecognized command %q", cmd.Command)
 	}
